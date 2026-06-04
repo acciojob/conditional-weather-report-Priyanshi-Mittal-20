f1bba6f6710169f4d36bca0fc5a72317a9b81e8f
Priyanshi Mittal <priyanshimzn05@gmail.com>
2026-06-04 12:08:37 +0530

    temp-3

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -3,12 +3,18 @@ import React from 'react'
 const WeatherDisplay = ({data}) => {
   return (
     <div>
-      <span>
-        {data.temperature>20 ? <p style={{color:"red"}}>{data.temperature}</p> : <p style={{color:"blue"}}>{data.temperature}</p> }
+      <span
+        style={{
+          color: data.temperature > 20 ? "red" : "blue"
+        }}
+      >
+        <p>Temperature: {data.temperature}</p>
       </span>
-      <p>{data.conditions}</p>
+
+      <p>Conditions: {data.conditions}</p>
     </div>
-  )
+  );
+
 }
 
 export default WeatherDisplay
